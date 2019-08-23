386ecc90caaefca1b3852c9706fc657b4e4f97a0
BlandineK <blandineuwase222@gmail.com>
2019-08-23 11:44:21 +0200

    add div class in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'Here are some quotes'
+  quotes :string[];
+
+  constructor(){
+    this.quotes = ['Do not cry because it is over,smile because it happened.','Be yourself everyone else is taken.','Life is like riding a bicycle.To keep your balance,you must keep moving.','There are only two waysto live your life.One is as though nothing is a miracle .The other is as though everything is a miracle.','You must be the change you wish to see in the world.','Never give up,trying to do what you really want to do,where there is love and inspiration,you cannot go wrong.']
+  }
 }
 

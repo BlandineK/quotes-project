5d288aa4d870648b9c97ffc23bd19fb8311a4fe4
BlandineK <blandineuwase222@gmail.com>
2019-08-23 12:16:37 +0200

    add quote new class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes :string[];
-
-  constructor(){
-    this.quotes = ['Do not cry because it is over,smile because it happened.','Be yourself everyone else is taken.','Life is like riding a bicycle.To keep your balance,you must keep moving.','There are only two waysto live your life.One is as though nothing is a miracle .The other is as though everything is a miracle.','You must be the change you wish to see in the world.','Never give up,trying to do what you really want to do,where there is love and inspiration,you cannot go wrong.']
-  }
+  quotes:Quote []= [
+    {id:1, name:'Do not cry because it is over,smile because it happened.'},
+    {id:2, name:'Be yourself everyone else is taken.'},
+    {id:3, name:'Life is like riding a bicycle.To keep your balance,you must keep moving.'},
+    {id:4, name:'There are only two ways to live your life. One is as though nothing is a miracle . The other is as though everything is a miracle.'},
+    {id:5, name:'You must be the change you wish to see in the world.'},
+    {id:6, name:'Never give up,trying to do what you really want to do,where there is love and inspiration,you cannot go wrong.'},
+  ];
 }
+ 
+
 

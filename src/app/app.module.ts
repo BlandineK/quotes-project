9235911cb95dc0177c3abcfa6e143260e8b99f11
BlandineK <blandineuwase222@gmail.com>
2019-08-27 10:29:16 +0200

    add changes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { Quotecomponent } from './quote/quote.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
@@ -12,15 +13,14 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent,
     QuoteDetailComponent,
+    Quotecomponent,7
     StrikethroughDirective,
     DateCountPipe,
     QuoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
